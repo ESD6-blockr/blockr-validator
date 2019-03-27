9255dc7f7273d5175e7fb349a3e386d17e2fef41
Hugo van de Vliert <hugovdv@outlook.com>
2019-03-27 09:22:11 +0100

    (blo 39) exception logging (#5)
    * Added npm start script
    
    * Added sentry module and npm start script
    
    * Changed Sentry package to node instead of js
    
    * Fixed merge issues on package lock
    
    * Added env var for sentry environment
    
    * Renamed import
    
    * Removed unconventional else statement

--- src/server.js
+++ src/server.js
@@ -11,8 +11,9 @@ const Security = require('./lib/security/security').getInstance();
 const KeyStorage = require('./lib/util/keyStorage');
 const TransactionBuilder = require('./lib/util/transactionBuilder').getInstance();
 const qrCodeGenerator = new QrCodeGenerator();
-const sleep = require('sleep');
+const Sleep = require('sleep');
 let io = require('socket.io');
+const Sentry = require('@sentry/node');
 
 let sender;
 let node;
@@ -23,17 +24,22 @@ const connected = false;
 const app = express();
 app.use(express.static(`${__dirname}/public`));
 
+// Setup exception logging
+const applicationMode = process.env.ENVIRONMENT;
+if (applicationMode === 'production' || applicationMode === 'staging') {
+    Sentry.init({
+        dsn: 'https://8ae89827146d49309d5a8e5699a605b6@sentry.io/1419602',
+        environment: applicationMode
+    });
+}
+
 // Redirect default '/' call to main.htm page
 app.get('/', (req, res) => {
     res.redirect('/index.html');
 });
 
 console.log("Start waiting....");
-if (process.env.IS_BACKUP === "true") {
-    sleep.sleep(10);
-} else {
-    sleep.sleep(15);
-}
+Sleep.sleep(process.env.IS_BACKUP === "true" ? 10 : 15);
 console.log("Stop waiting....");
 
 // Start listening with HTTP (picks random available port)
