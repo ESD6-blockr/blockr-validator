6f56f4130cc301c121dc3cb752c1447c0d41ca1f
Unknown <t.fudala@student.fontys.nl>
2019-05-16 11:34:38 +0200

    Add constants

--- src/stores/constant.store.ts
+++ src/stores/constant.store.ts
@@ -11,12 +11,16 @@ export class ConstantStore {
     public GENESIS_STAKE_AMOUNT: number;
     /* The public key of the admin wallet */
     public ADMIN_PUBLIC_KEY: string;
+    /* The currently configured public key that the validator should mine for */
+    public VALIDATOR_PUBLIC_KEY: string;
     /* Block reward */
     public BLOCK_REWARD_AMOUNT: number;
     /* Gensis block number */
     public GENESIS_BLOCK_NUMBER: number;
     /* The file path of the .keys file */
     public KEYS_FILE_PATH: string;
+    /* The file path of the .env file */
+    public ENV_FILE_PATH: string;
     /* The current version of the validator */
     public VALIDATOR_VERSION: string;
     /* The Sentry DSN */
@@ -27,10 +31,12 @@ export class ConstantStore {
     public constructor() {
         this.GENESIS_COIN_AMOUNT = 900_000_000;
         this.GENESIS_STAKE_AMOUNT = 1;
-        this.ADMIN_PUBLIC_KEY = process.env.ADMIN_PUBLIC_KEY || "";
+        this.ADMIN_PUBLIC_KEY = "";
+        this.VALIDATOR_PUBLIC_KEY = process.env.VALIDATOR_PUBLIC_KEY || "";
         this.BLOCK_REWARD_AMOUNT = 10;
         this.GENESIS_BLOCK_NUMBER = 1;
         this.KEYS_FILE_PATH = `${join(__dirname, "../../")}.keys`;
+        this.ENV_FILE_PATH = `${join(__dirname, "../../")}.env`;
         this.VALIDATOR_VERSION = process.env.VALIDATOR_VERSION || "";
         this.SENTRY_DSN = process.env.SENTRY_DSN || "";
         this.SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT || "";
