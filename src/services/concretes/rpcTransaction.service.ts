03091c2574cf965ded72793fd7c9a80a79d1d49c
Unknown <t.fudala@student.fontys.nl>
2019-06-05 09:47:32 +0200

    Organize imports in RpcTransaction

--- src/services/concretes/rpcTransaction.service.ts
+++ src/services/concretes/rpcTransaction.service.ts
@@ -1,19 +1,20 @@
 import { Transaction } from "@blockr/blockr-models";
-import * as protoLoader from "@grpc/proto-loader";
-import * as grpc from "grpc";
+import { loadSync } from "@grpc/proto-loader";
+import { loadPackageDefinition, Server, ServerCredentials } from "grpc";
 import { injectable } from "inversify";
 
-const PROTO_PATH = "../../utils/transactions.proto";
+const PROTOCOL_PATH = "../../utils/transactions.proto";
 const HOST = "127.0.0.1";
 const PORT = "5678";
 
 @injectable()
 export class RpcTransactionService {
+  // No type exists for this property as it is defined in the protocol (.proto) file, hence any.
   private transactionProto: any;
 
   constructor() {
-    const packageDefinition = protoLoader.loadSync(
-      PROTO_PATH,
+    const packageDefinition = loadSync(
+      PROTOCOL_PATH,
       {
         defaults: true,
         enums: String,
@@ -21,16 +22,16 @@ export class RpcTransactionService {
         longs: String,
         oneofs: true,
       });
-    this.transactionProto = grpc.loadPackageDefinition(packageDefinition).transactions;
+    this.transactionProto = loadPackageDefinition(packageDefinition).transactions;
   }
 
-  public getServer(): grpc.Server {
-    const server = new grpc.Server();
+  public getServer(): Server {
+    const server = new Server();
     server.addProtoService(this.transactionProto.TransactionRpcService.service, {
       addTransaction: this.addTransaction,
     });
 
-    server.bind(`${HOST}:${PORT}`, grpc.ServerCredentials.createInsecure());
+    server.bind(`${HOST}:${PORT}`, ServerCredentials.createInsecure());
     server.start();
     return server;
   }
