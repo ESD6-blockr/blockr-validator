271fd6eb278bc795419e8f0456089f79ab88f508
Unknown <t.fudala@student.fontys.nl>
2019-06-06 17:16:06 +0200

    Fix typo in IBlockServiceAdapter

--- src/adapters/interfaces/blockService.adapter.ts
+++ src/adapters/interfaces/blockService.adapter.ts
@@ -2,5 +2,5 @@ import { Block } from "@blockr/blockr-models";
 import { IBaseServiceAdapter } from "./baseService.adapter";
 
 export interface IBlockServiceAdapter extends IBaseServiceAdapter {
-    addPendingProposedBLockAsync(proposedBlock: Block): Promise<void>;
+    addPendingProposedBlockAsync(proposedBlock: Block): Promise<void>;
 }
