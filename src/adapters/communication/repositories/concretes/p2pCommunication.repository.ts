60122fc79aeafd7e64cb908f4d7dcf13659a0d3a
Unknown <t.fudala@student.fontys.nl>
2019-06-08 10:47:25 +0200

    Revert "Remove console.log"
    This reverts commit c29acaadfb1cfbaceca2e230bd8b6dc823cfbc24.

--- src/adapters/communication/repositories/concretes/p2pCommunication.repository.ts
+++ src/adapters/communication/repositories/concretes/p2pCommunication.repository.ts
@@ -46,6 +46,8 @@ export class P2PCommunicationRepository implements ICommunicationRepository {
     }
 
     public getPeerOfType(peerType: PeerType): [string, string] | undefined {
+        // tslint:disable-next-line: no-console
+        console.log("getPeerOfType", this.peer);
         return this.peer.getPeerOfType(peerType);
     }
 
