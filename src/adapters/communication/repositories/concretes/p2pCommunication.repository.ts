c29acaadfb1cfbaceca2e230bd8b6dc823cfbc24
Unknown <t.fudala@student.fontys.nl>
2019-06-08 10:47:25 +0200

    Remove console.log

--- src/adapters/communication/repositories/concretes/p2pCommunication.repository.ts
+++ src/adapters/communication/repositories/concretes/p2pCommunication.repository.ts
@@ -46,8 +46,6 @@ export class P2PCommunicationRepository implements ICommunicationRepository {
     }
 
     public getPeerOfType(peerType: PeerType): [string, string] | undefined {
-        // tslint:disable-next-line: no-console
-        console.log("getPeerOfType", this.peer);
         return this.peer.getPeerOfType(peerType);
     }
 
