ed8340a5c9fa1c99ebfa1ba209a9d7e6d1b353a0
Unknown <t.fudala@student.fontys.nl>
2019-06-18 11:31:59 +0200

    Fix

--- src/services/concretes/victoriousBlock.service.ts
+++ src/services/concretes/victoriousBlock.service.ts
@@ -11,7 +11,6 @@ export class VictoriousBlockService implements IVictoriousBlockServiceAdapter {
     private readonly dataAccessLayer: DataAccessLayer;
     private readonly stateService: StateService;
     private readonly victoriousBlockAdapter: VictoriousBlockAdapter;
-    private isActive: boolean = false;
     private lastProccesedBlockNummer: number = -1;
 
     constructor(@inject(DataAccessLayer) dataAccessLayer: DataAccessLayer,
@@ -29,7 +28,7 @@ export class VictoriousBlockService implements IVictoriousBlockServiceAdapter {
             // Since the BlockJob's cycle execution is synced for every Node, all victorious blocks that will 
             // be received while this service is already processing a victorious block, are identical to the one that is
             // currently being processed. Hence the if-statement and the early resolve.
-            if (this.isActive || victoriousBlock.blockHeader.blockNumber <= this.lastProccesedBlockNummer) {
+            if (victoriousBlock.blockHeader.blockNumber <= this.lastProccesedBlockNummer) {
                 logger.info("[VictoriousBlockService] Skipping received victorious block " +
                             "because the service is already active.");
 
@@ -37,7 +36,6 @@ export class VictoriousBlockService implements IVictoriousBlockServiceAdapter {
                 return;
             }
             this.lastProccesedBlockNummer = victoriousBlock.blockHeader.blockNumber;
-            this.isActive = true;
 
             try {
                 await this.dataAccessLayer.addBlockAsync(victoriousBlock);
@@ -48,8 +46,6 @@ export class VictoriousBlockService implements IVictoriousBlockServiceAdapter {
             } catch (error) {
 
                 reject(error);
-            } finally {
-                this.isActive = false;
             }
         });
     }
